d5b3909ab1a453f5aa2ee07b33306ac970fb9b07
Datkayim <dkeldibekova08@gmail.com>
2023-05-17 16:01:44 +0600

    Initial Commit

---
+++ Month5HWs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Month5HWs
+//
+//  Created by Jarae on 17/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
